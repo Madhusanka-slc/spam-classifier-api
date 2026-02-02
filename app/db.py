2aa1a2e131cd0a1301189e5a1effaf46e5b3713f
Chathura Madhusanka <madhusanka.slc@gmail.com>
2026-02-02 21:10:03 +0530

    change get session db

--- app/db.py
+++ app/db.py
@@ -1,33 +1,33 @@
-from . import config
 import pathlib
-
+from . import config
 from cassandra.cluster import Cluster
 from cassandra.auth import PlainTextAuthProvider
 from cassandra.cqlengine import connection
 
-
 BASE_DIR = pathlib.Path(__file__).resolve().parent
-SOURCE_DIR = BASE_DIR / "ignored"
+SOURCE_DIR = BASE_DIR / 'ignored'
 if not SOURCE_DIR.exists():
-    SOURCE_DIR = BASE_DIR / "decrypted"
-CLUSTER_BUNDLE = str(SOURCE_DIR / "astradb_connect.zip")
+    SOURCE_DIR = BASE_DIR / 'decrypted'
+
+CLUSTER_BUNDLE = str( SOURCE_DIR / 'astradb_connect.zip')
 
 settings = config.get_settings()
 
-ASTRA_DB_CLIENT_ID = settings.astra_db_client_id
-ASTRA_DB_CLIENT_SECRET = settings.astra_db_client_secret
+ASTRA_DB_CLIENT_ID = settings.db_client_id
+ASTRA_DB_CLIENT_SECRET  = settings.db_client_secret
+
 
 def get_cluster():
-    cloud_config = {
+    cloud_config= {
         'secure_connect_bundle': CLUSTER_BUNDLE
     }
     auth_provider = PlainTextAuthProvider(ASTRA_DB_CLIENT_ID, ASTRA_DB_CLIENT_SECRET)
-    cluster = Cluster(cloud=cloud_config, auth_provider=auth_provider, protocol_version=4)
-    return cluster
+    return Cluster(cloud=cloud_config, auth_provider=auth_provider)
+    
 
 def get_session():
     cluster = get_cluster()
     session = cluster.connect()
-    connection.register_connection('astra', session=session)
-    connection.set_default_connection('astra')
+    connection.register_connection(str(session), session=session)
+    connection.set_default_connection(str(session))
     return session
